4d049ff5a92c1ab1ad9eab7e27ea24d994b4d3c3
mirkoebert <mirko.ebert@gmail.com>
2015-01-05 17:52:56 +0100

    add parameter to custom gradle task

--- src/main/java/com/ebertp/gradleplugin/wpt/WptTask.java
+++ src/main/java/com/ebertp/gradleplugin/wpt/WptTask.java
@@ -5,9 +5,20 @@ import org.gradle.api.tasks.TaskAction;
 
 public class WptTask extends DefaultTask {
 	
+	private String wptutl = "www.ebert-p.com";
+	private int maxdoccomlete = 600;
+	
 	@TaskAction
     public void wptTask() {
-        System.out.println("Hello from wptTask.");
+        System.out.println("Java: Hello from wptTask: "+wptutl+" "+maxdoccomlete);
     }
+	
+	public void wpturl(String para){
+		wptutl = para;
+	}
+	
+	public void maxdoccomlete(String para){
+		maxdoccomlete = Integer.parseInt(para);
+	}
 
 }
